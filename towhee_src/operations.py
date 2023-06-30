ca3a87a13dfb7b77e7b39e6e48e3dbd70d23beb5
Jael Gu <mengjia.gu@zilliz.com>
2023-06-30 17:27:47 +0800

    Add configs for more LLMs
    Signed-off-by: Jael Gu <mengjia.gu@zilliz.com>

--- towhee_src/operations.py
+++ towhee_src/operations.py
@@ -29,7 +29,8 @@ def chat(session_id, project, question):
         messages = [(question, final_answer)]
         memory_store.add_history(project, session_id, messages)
         return final_answer
-    except Exception:
+    except Exception as e:
+        print(111, e)
         return 'Something went wrong. Please clear history and try again!'
 
 
@@ -102,31 +103,31 @@ def clear_history(project, session_id):
         raise RuntimeError(f'Failed to clear memory:\n{e}')
 
 
-if __name__ == '__main__':
-    project = 'akcio'
-    data_src = 'https://docs.towhee.io/'
-    session_id = 'test000'
-    question0 = 'What is your code name?'
-    question1 = 'What is Towhee?'
-    question2 = 'What does it do?'
+# if __name__ == '__main__':
+#     project = 'akcio'
+#     data_src = 'https://docs.towhee.io/'
+#     session_id = 'test000'
+#     question0 = 'What is your code name?'
+#     question1 = 'What is Towhee?'
+#     question2 = 'What does it do?'
 
-    # count = insert(data_src=data_src, project=project, source_type='url')
-    # print('\nCount:', count)
-    # print('\nCheck:', check(project))
+#     count = insert(data_src=data_src, project=project, source_type='url')
+#     print('\nCount:', count)
+#     print('\nCheck:', check(project))
     
-    # answer = chat(project=project, session_id=session_id, question=question0)
-    # print('\nAnswer:', answer)
+#     answer = chat(project=project, session_id=session_id, question=question0)
+#     print('\nAnswer:', answer)
 
-    # answer = chat(project=project, session_id=session_id, question=question1)
-    # print('\nAnswer:', answer)
+#     answer = chat(project=project, session_id=session_id, question=question1)
+#     print('\nAnswer:', answer)
 
-    # answer = chat(project=project, session_id=session_id, question=question2)
-    # print('\nAnswer:', answer)
-    # print('\nHistory:', get_history(project, session_id))
+#     answer = chat(project=project, session_id=session_id, question=question2)
+#     print('\nAnswer:', answer)
+#     print('\nHistory:', get_history(project, session_id))
 
-    # clear_history(project, session_id)
-    # print('\nHistory:', get_history(project, session_id))
+#     clear_history(project, session_id)
+#     print('\nHistory:', get_history(project, session_id))
 
-    print('\nDropping project ...')
-    drop(project=project)
-    print(check(project))
+#     print('\nDropping project ...')
+#     drop(project=project)
+#     print(check(project))
